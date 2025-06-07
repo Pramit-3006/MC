4d92dea67f494b3170b0dd2ce5ddaa649c9c40cc
PRAMIT PRADHAN <pradhanpramit3006@gmail.com>
2025-06-07 23:05:46 +0530

    Update and rename config.py to rag_engine.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mentors_connect.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'static/uploads'
-    ALLOWED_EXTENSIONS = {'pdf', 'docx'}
